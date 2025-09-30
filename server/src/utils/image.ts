e92c5a2cdf363f4311c4fcd81b8be39990afcf61
qiyk <qiyk@glodon.com>
2025-09-30 09:45:05 +0800

    fix:预览显示下载路径问题1

--- server/src/utils/image.ts
+++ server/src/utils/image.ts
@@ -4,6 +4,9 @@ export function extractImage(content: string) {
     let avatar: string | undefined = undefined;
     if (img_match) {
         avatar = img_match[1];
+        console.log('Extracted avatar:', avatar);
+    } else {
+        console.log('No image found in content:', content.substring(0, 200));
     }
     return avatar;
 }
